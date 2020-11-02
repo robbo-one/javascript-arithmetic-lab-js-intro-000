ec5f1b719f178605da596a2dd6b153e201ba3451
R C <robsc801@gmail.com>
2020-11-02 03:30:21 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var number;
+function add(a, b){
+  number = a + b;
+  return number;
+}
+  function subtract(a, b) {
+    number = a - b;
+    return number;
+  }
+  function multiply(a, b) {
+    number = a * b;
+    return number;
+  }
+  function divide(a, b) {
+    number = a / b;
+    return number;
+  }
